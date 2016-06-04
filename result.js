518d6539d239dd825359d562e8c54e5dfe61f8d6
emascarinas <emascarinas@users.noreply.github.com>
2016-06-04 14:28:42 -0700

    Add files via upload

--- result.js
+++ result.js
@@ -1,18 +1,18 @@
 
-var typeLabel = ['501 Straight 1','501 Straight 2','501 Straight 3','501 Straight 1']
-var mySwiper = new Swiper ('.swiper-container', {
+$(function() {
+
+	var typeLabel = ['501 Straight 1','501 Straight 2','501 Straight 3','501 Straight 1']
+	var mySwiper = new Swiper ('.swiper-container', {
     // Optional parameters
     loop: true,
-    
-    
     // Navigation arrows
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
     onSlideChangeEnd : function(swiperHere) {
-    	$(function() {
-    		var index = parseInt($('.swiper-wrapper .swiper-slide-active').data('swiper-slide-index'));
-    		$('.type-label').text(typeLabel[index]);
-    	});
-    }
+    	var index = parseInt($('.swiper-wrapper .swiper-slide-active').data('swiper-slide-index'));
+    	$('.type-label').text(typeLabel[index]);
+    },
+});
+	$(".sticky-pants").stick_in_parent();
 
 });
