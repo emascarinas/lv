45398a2cd611475ac42436844b0b6c86a485cdb9
emascarinas <emascarinas@users.noreply.github.com>
2016-06-06 15:47:02 -0700

    Add files via upload

--- result.js
+++ result.js
@@ -11,10 +11,8 @@ var swiper = new Swiper('.swiper-container-head', {
 //pants slider swiper
 var typeLabel = ['501 Straight 1','501 Straight 2','501 Straight 3','501 Straight 1'];
 var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
     loop: true,
     effect: 'flip',
-    // Navigation arrows
     nextButton: '.swiper-button-next',
     prevButton: '.swiper-button-prev',
     onSlideChangeEnd : function(swiperHere) {
@@ -22,15 +20,6 @@ var mySwiper = new Swiper ('.swiper-container', {
     	$('.type-label').text(typeLabel[index]);
     },
 });
-var swiperThread = new Swiper ('.swiper-container-thread', {
-	loop: true,
-	effect: 'fade',
-});
-
-// var swiperColor = new Swiper ('.swiper-container-color', {
-// 	loop: true,
-// 	effect: 'fade',
-// });
 
 var swiperColorB = new Swiper ('.swiper-container-color-b', {
 	loop: true,
@@ -41,69 +30,76 @@ var swiperColorB = new Swiper ('.swiper-container-color-b', {
 $(".sticky-pants").stick_in_parent({parent: '#result'});
 
 // distress
-var isNeedle = true;
-var needleIndex = 1;
-var cutterIndex = 0;
-var needleMax = 3;
-var cutterMax = 2;
 
-$('.needle').on('click',function(){
-	isNeedle = true;
-	needleIndex = 1;
-});
 
-$('.cutter').on('click',function(){
-	isNeedle = false;
-	cutterIndex = 0;
-});
+// var isNeedle = true;
+// var needleIndex = 1;
+// var cutterIndex = 0;
+// var needleMax = 3;
+// var cutterMax = 2;
 
-$(".swipe-area").swipe( {
-	swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-		if(direction == 'right') {
-			if(isNeedle && needleIndex > 1){
-				needleIndex--;
-				needleSwap(needleIndex);
-			}
-			if(!isNeedle && cutterIndex > 1){
-				cutterIndex--;
-				cutterSwap(cutterIndex);
-			}
-		}
-		else {
-			if(isNeedle && needleIndex < needleMax){
-				needleIndex++;
-				needleSwap(needleIndex);
-			}
-			if(!isNeedle && cutterIndex < cutterMax){
-				cutterIndex++;
-				cutterSwap(cutterIndex);
-			}
-		}
+// $('.needle').on('click',function(){
+// 	isNeedle = true;
+// 	needleIndex = 1;
+// });
 
-	}
-});
+// $('.cutter').on('click',function(){
+// 	isNeedle = false;
+// 	cutterIndex = 0;
+// });
 
-var swiperDistress = new Swiper ('.swiper-container-distress', {
-	loop: true,
-	effect: 'fade',
-});
-$('.needleB').on('click',function(){
-	swiperDistress.removeAllSlides();
-	swiperDistress.prependSlide([
-		'<div class="swiper-slide"><img width="325" src="images/distress_1.png"></div>', 
-		'<div class="swiper-slide"><img width="325" src="images/distress_2.png"></div>',
-		'<div class="swiper-slide"><img width="325" src="images/distress_3.png"></div>'
-		]);
-});
-$('.cutterB').on('click',function(){
-	swiperDistress.removeAllSlides();
-	swiperDistress.prependSlide([
-		'<div class="swiper-slide"><img width="325" src="images/distress_4.png"></div>', 
-		'<div class="swiper-slide"><img width="325" src="images/distress_5.png"></div>'
-		]);
-});
+// $(".swipe-area").swipe( {
+// 	swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+// 		if(direction == 'right') {
+// 			if(isNeedle && needleIndex > 1){
+// 				needleIndex--;
+// 				needleSwap(needleIndex);
+// 			}
+// 			if(!isNeedle && cutterIndex > 1){
+// 				cutterIndex--;
+// 				cutterSwap(cutterIndex);
+// 			}
+// 		}
+// 		else {
+// 			if(isNeedle && needleIndex < needleMax){
+// 				needleIndex++;
+// 				needleSwap(needleIndex);
+// 			}
+// 			if(!isNeedle && cutterIndex < cutterMax){
+// 				cutterIndex++;
+// 				cutterSwap(cutterIndex);
+// 			}
+// 		}
+
+// 	}
+// });
+
+// var swiperDistress = new Swiper ('.swiper-container-distress', {
+// 	loop: true,
+// 	effect: 'fade',
+// });
+// $('.needleB').on('click',function(){
+// 	swiperDistress.removeAllSlides();
+// 	swiperDistress.prependSlide([
+// 		'<div class="swiper-slide"><img width="325" src="images/distress_1.png"></div>', 
+// 		'<div class="swiper-slide"><img width="325" src="images/distress_2.png"></div>',
+// 		'<div class="swiper-slide"><img width="325" src="images/distress_3.png"></div>'
+// 		]);
+// });
+// $('.cutterB').on('click',function(){
+// 	swiperDistress.removeAllSlides();
+// 	swiperDistress.prependSlide([
+// 		'<div class="swiper-slide"><img width="325" src="images/distress_4.png"></div>', 
+// 		'<div class="swiper-slide"><img width="325" src="images/distress_5.png"></div>'
+// 		]);
+// });
 
 //color thread
+var swiperThread = new Swiper ('.swiper-container-thread-b', {
+	// loop: true,
+	effect: 'fade',
+});
+
 $('#range-bright').rangeslider({polyfill: false}).on('input', function() {
 	swiperColorB.slideNext();
 });
@@ -111,10 +107,12 @@ $('#range-fade').rangeslider({polyfill: false}).on('input', function() {
 	swiperColorB.slidePrev();
 });
 $('#range-thread').rangeslider({polyfill: false}).on('input', function() {
-	swiperThread.slideNext();
+	console.log(Math.round((this.value/100)*10));
+	swiperThread.slideTo(Math.round((this.value/100)*10));
 });
 
 //pocket
+
 $('#range-pocket').rangeslider({polyfill: false}).on('input', function() {
 	console.log(this.value);
 });
